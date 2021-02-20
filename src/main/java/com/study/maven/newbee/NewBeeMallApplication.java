72c6efcbabe6b189053581d2b3a37cb227ea7405
helinhai <17703595860@163.com>
2021-02-20 15:19:39 +0800

    添加项目依赖

---
+++ src/main/java/com/study/maven/newbee/NewBeeMallApplication.java
@@ -0,0 +1,16 @@
+package com.study.maven.newbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HLH
+ * @email 17703595860@163.com
+ * @date : Created in  2021/2/20 14:33
+ */
+@SpringBootApplication
+public class NewBeeMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeeMallApplication.class, args);
+    }
+}
