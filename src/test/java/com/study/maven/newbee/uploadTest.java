1388bf02e26473fafd0c70b6e3ef2b2363299046
helinhai <17703595860@163.com>
2021-03-09 19:36:02 +0800

    放入前台页面

--- src/test/java/com/study/maven/newbee/uploadTest.java
+++ src/test/java/com/study/maven/newbee/uploadTest.java
@@ -1,5 +1,13 @@
 package com.study.maven.newbee;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,6 +15,16 @@ import static org.junit.Assert.*;
  * @email 17703595860@163.com
  * @date : Created in  2021/2/20 15:26
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class uploadTest {
+
+    @Autowired
+    ApplicationContext ac;
+
+    @Test
+    public void test(){
+        System.out.println(ac);
+    }
 
 }
