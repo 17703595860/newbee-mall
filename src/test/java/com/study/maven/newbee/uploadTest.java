eb17367494e280b89cdccbd420956eb1d05061d8
helinhai <17703595860@163.com>
2021-03-09 19:35:28 +0800

    '放入前台页面'

---
+++ src/test/java/com/study/maven/newbee/uploadTest.java
@@ -0,0 +1,12 @@
+package com.study.maven.newbee;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author HLH
+ * @email 17703595860@163.com
+ * @date : Created in  2021/2/20 15:26
+ */
+public class uploadTest {
+
+}
